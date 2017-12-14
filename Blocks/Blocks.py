dad3ba3875d9f8066437c3aa5328e32d664c3529
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2017-12-14 05:52:17 +0000

    Geri score

--- Blocks/Blocks.py
+++ Blocks/Blocks.py
@@ -6,7 +6,7 @@ wH = 900
 
 
 class game:
-    def __init__(self,wW,wH,fjoldi):
+    def __init__(self,wW,wH,fjoldi,nafn,diff):
         self.screen = pygame.display.set_mode((wW, wH))
         pygame.display.set_caption("Blocks")
         self.clock = pygame.time.Clock()
@@ -17,7 +17,9 @@ class game:
         self.done = False
         self.speed = 3
         self.orange = (255, 100, 0)
-        self.blue = (0,0,255)
+        self.blue = (0, 0, 255)
+        self.red = (255, 0, 0)
+        self.white = (255,255,255)
         self.boxes = []
         self.tel = 0
         self.fjoldi = fjoldi
@@ -26,16 +28,53 @@ class game:
         self.tap = False
         self.score = 0
         self.blackS = False
+        self.nafn = nafn
+        self.diff = diff
 
-
+        #print(self.nafn)
+        with open("score.txt","r", encoding="UTF-8") as f:
+            try:
+                self.levels = []
+                for a in range(4):
+                    if self.diff == a+1:
+                        self.dict = f.readline()
+                        self.levels.append(self.dict)
+                        self.dict = eval(self.dict)
+                    else:
+                        self.levels.append(f.readline())
+            except:
+                self.dict = {}
+        try:
+            self.levels.remove('')
+        except:
+            pass
+        print(self.levels)
+        self.Sscore = 0
+        print(self.dict)
+        for a in self.dict:
+            if self.dict[a] > self.Sscore:
+                self.Sscore = self.dict[a]
+                self.Snafn = a
+        print("Sscore",self.Sscore)
+        print("Snafn",self.Snafn)
     def thing(self,x):
         print(len(self.boxes))
         if self.tel % self.fjoldi == 0:
-            pygame.draw.rect(self.screen, self.orange,pygame.Rect(x, -60, 60, 60))
+            if self.blackS == False:
+                pygame.draw.rect(self.screen, self.orange,pygame.Rect(x, -60, 60, 60))
+            elif self.blackS == True and self.score >= 50:
+                pygame.draw.rect(self.screen, self.white, pygame.Rect(x, -60, 60, 60))
+            elif self.blackS == True:
+                pygame.draw.rect(self.screen, self.blue, pygame.Rect(x, -60, 60, 60))
             box = [x, -60]
             self.boxes.append(box)
         for a in range(len(self.boxes)):
-            pygame.draw.rect(self.screen,self.orange, pygame.Rect(self.boxes[a][0],self.boxes[a][1],60,60))
+            if self.blackS == False:
+                pygame.draw.rect(self.screen,self.orange, pygame.Rect(self.boxes[a][0],self.boxes[a][1],60,60))
+            elif self.blackS == True and self.score >= 50:
+                pygame.draw.rect(self.screen, self.white, pygame.Rect(self.boxes[a][0], self.boxes[a][1], 60, 60))
+            elif self.blackS == True:
+                pygame.draw.rect(self.screen, self.blue, pygame.Rect(self.boxes[a][0], self.boxes[a][1], 60, 60))
             self.boxes[a].append(self.boxes[a][1]+self.speed)
             self.boxes[a].remove(self.boxes[a][1])
         temp = self.boxes
@@ -47,8 +86,6 @@ class game:
         for a in self.boxes:
             x = a[0]
             y = a[1]
-
-
             if (self.x + 56 >= x and self.y + 56 >= y) and (self.x <= x + 56 and self.y <= y + 56):
                 game.lost(self)
                 break
@@ -58,8 +95,6 @@ class game:
         else:
             label = self.font.render("Game Paused", 1, (255, 255, 255))
         self.screen.blit(label,((self.wW / 2) - 120, (self.wH / 2) - 50))
-        pygame.display.flip()
-        self.clock.tick(60)
     def lost(self):
         self.tap = True
         if self.blackS == False:
@@ -67,8 +102,22 @@ class game:
         else:
             label = self.font.render("You Lost!", 1, (255, 255, 255))
         self.screen.blit(label,((self.wW / 2) - 120, (self.wH / 2) - 50))
-        pygame.display.flip()
-        self.clock.tick(60)
+
+        try:
+            if self.dict[self.nafn]  > self.Sscore:
+                self.dict[self.nafn] = self.score
+            else:
+                self.dict[self.nafn] = self.Sscore
+        except:
+            self.dict[self.nafn] = self.score
+        print(self.dict)
+        with open("score.txt","w", encoding="UTF-8") as f:
+            for a in range(4):
+                if self.diff == a+1:
+                    f.writelines(str(self.dict)+"\n")
+                else:
+                    f.writelines(self.levels[a])
+
         
     def loop(self):
         Stel = 0
@@ -82,7 +131,10 @@ class game:
 
                 if self.score < 30:
                     self.screen.fill((255, 255, 255))
-                elif self.score >= 30:
+                elif self.score >= 30 and self.score < 50:
+                    self.screen.fill((70, 70, 70))
+                    self.blackS = True
+                elif self.score >= 50:
                     self.blackS = True
                     self.screen.fill((0, 0, 0))
 
@@ -96,9 +148,10 @@ class game:
                 if self.x < self.wW - 60:
                     if pressed[pygame.K_RIGHT]: self.x += self.speed + 1
 
-
-                pygame.draw.rect(self.screen, self.blue, pygame.Rect(self.x, self.y, 60, 60))
-
+                if self.blackS == False:
+                    pygame.draw.rect(self.screen, self.blue, pygame.Rect(self.x, self.y, 60, 60))
+                elif self.blackS == True:
+                    pygame.draw.rect(self.screen, self.red, pygame.Rect(self.x, self.y, 60, 60))
                 game.thing(self,random.randint(0,wW-60))
 
                 if Stel % 120 == 0 and Stel != 0:
@@ -111,7 +164,18 @@ class game:
                     label = self.font.render(str(self.score), 1, (255, 255, 255))
                 self.screen.blit(label, (0, 0))
 
-                pygame.display.flip()
+                try:
+                    if self.blackS == False:
+                        hscore = self.font.render(self.Snafn + ":" + str(self.Sscore), 1, (0, 0, 0))
+                    else:
+                        hscore = self.font.render(self.Snafn + ":" + str(self.Sscore), 1, (255, 255, 255))
+                except:
+                    if self.blackS == False:
+                        hscore = self.font.render("Enginn",1,(0, 0, 0))
+                    else:
+                        hscore = self.font.render("Enginn", 1, (255, 255, 255))
+                self.screen.blit(hscore,(0,self.wH-60))
+
                 self.clock.tick(120)
                 game.check(self)
             if pressed[pygame.K_r]:
@@ -124,44 +188,71 @@ class game:
                 elif self.fjoldi == 10:
                     shard()
             if pressed[pygame.K_ESCAPE] and self.tap == False:
-
+                game.pause(self)
                 if self.gP == False:
                     self.gP = True
                 elif self.gP == True:
                     self.gP = False
-                game.pause(self)
                 time.sleep(0.15)
+            pygame.display.flip()
 
 
 
 def easy():
+    global oldname
     try:
-        root.destroy()
+        nafn = str(entry_1.get())
+        oldname = nafn
     except:
-        pass
-    h = game(wW,wH,40)
-    h.loop()
-def medium():
-    try:
-        root.destroy()
-    except:
-        pass
-    h = game(wW, wH, 30)
-    h.loop()
-def hard():
-    try:
-        root.destroy()
-    except:
-        pass
-    h = game(wW, wH, 20)
-    h.loop()
-def shard():
+        nafn = oldname
     try:
         root.destroy()
     except:
         pass
 
-    h = game(wW, wH, 10)
+    h = game(wW,wH,40, nafn,1)
+    h.loop()
+def medium():
+    global oldname
+    try:
+        nafn = str(entry_1.get())
+        oldname = nafn
+    except:
+        nafn = oldname
+    try:
+        root.destroy()
+    except:
+        pass
+
+    h = game(wW, wH, 30, nafn,2)
+    h.loop()
+def hard():
+    global oldname
+    try:
+        nafn = str(entry_1.get())
+        oldname = nafn
+    except:
+        nafn = oldname
+    try:
+        root.destroy()
+    except:
+        pass
+
+    h = game(wW, wH, 20, nafn,3)
+    h.loop()
+def shard():
+    global oldname
+    try:
+        nafn = str(entry_1.get())
+        oldname = nafn
+    except:
+        nafn = oldname
+    try:
+        root.destroy()
+    except:
+        pass
+
+    h = game(wW, wH, 10, nafn,4)
     h.loop()
 
 root = Tk()
@@ -169,10 +260,14 @@ button_1 = Button(root, text=" Easy ", command=easy)
 button_2 = Button(root, text=" Medium ", command=medium)
 button_3 = Button(root, text=" Hard ", command=hard)
 button_4 = Button(root, text=" Superhard ",command = shard)
+entry_1 = Entry(root, textvariable=StringVar())
+label = Label(root,text = "Name: ")
 
+label.pack()
+entry_1.pack()
+button_1.pack()
+button_2.pack()
+button_3.pack()
+button_4.pack()
 
-button_1.grid(row = 1,column = 1)
-button_2.grid(row = 1,column = 2)
-button_3.grid(row = 1,column = 3)
-button_4.grid(row = 1,column = 4)
 root.mainloop()
