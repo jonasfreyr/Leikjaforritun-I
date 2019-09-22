1a622c077297e08ff7a300963c4e98aef6ad9af9
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2019-09-22 00:13:19 +0000

    Fixa smá bug í lineLine

--- Shooter_2/settings.py
+++ Shooter_2/settings.py
@@ -1,15 +1,19 @@
 from vector import *
 
 def lineLine(x1, y1, x2, y2, x3, y3, x4, y4):
-    uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+    try:
+        uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
 
-    uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+        uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
 
-    if (uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1):
+        if (uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1):
+            return True
+
+        else:
+            return False
+
+    except ZeroDivisionError:
         return True
-
-    else:
-        return False
 
 # Game
 FPS = 120
